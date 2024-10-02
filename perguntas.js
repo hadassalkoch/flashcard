dc149e8185d938839d01771975863623b95bdc68
hadassalkoch <159554898+hadassalkoch@users.noreply.github.com>
2024-10-02 07:52:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'linguagem formal',
+    'O que é linguagem formal',
+    '[uma linguagem que tem regras'
+)
+
+criaCartao(
+    'Geografia',
+    'O que e lugar?',
+    'area menor com mais afinidade'
+)
+
+criaCartao(
+    'portugues',
+    'Quem escreveu o poema amor e medo?',
+    'Casimiro de Abreu'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa home?',
+    'casa'
+)
+criaCartao(
+    'matematica',
+    'quanto e dois mais dois',
+    '[quatro]'
+)
+
+criaCartao(
+    'ingles',
+    'O que significa pink',
+    'rosa'
+)
+
+criaCartao(
+    'portugues',
+    'Quando se usa ? na frase',
+    'quando e uma pergunta'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa cat?',
+    'gato'
+)
+criaCartao(
+    'portugues',
+    'Quando se usa ? na frase',
+    'quando e uma pergunta'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que significa stop?',
+    'pare'
+)
